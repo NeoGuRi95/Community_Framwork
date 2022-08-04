547885086516244207b6ff1dbb5a72ecd4a2bfcd
NeoGuRi95 <jea5158@gmail.com>
2022-08-04 13:40:06 +0900

    first commit

---
+++ src/main/java/com/ll/exam/article/service/ArticleService.java
@@ -0,0 +1,38 @@
+package com.ll.exam.article.service;
+
+import com.ll.exam.annotation.Autowired;
+import com.ll.exam.annotation.Service;
+import com.ll.exam.article.dto.ArticleDto;
+import com.ll.exam.article.repository.ArticleRepository;
+
+import java.util.List;
+
+@Service
+public class ArticleService {
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    public List<ArticleDto> getArticles() {
+        return articleRepository.getArticles();
+    }
+
+    public ArticleDto getArticleById(long id) {
+        return articleRepository.getArticleById(id);
+    }
+
+    public long getArticlesCount() {
+        return articleRepository.getArticlesCount();
+    }
+
+    public long write(String title, String body, boolean isBlind) {
+        return articleRepository.write(title, body, isBlind);
+    }
+
+    public void modify(int id, String title, String body, boolean b) {
+        articleRepository.modify(id, title, body, b);
+    }
+
+    public void delete(int id) {
+        articleRepository.delete(id);
+    }
+}
---
+++ src/test/java/com/ll/exam/AppTest.java
@@ -0,0 +1,6 @@
+package com.ll.exam;
+
+public class AppTest {
+
+}
+
