fc131f8da8224b1458f2609d5564828da4a40c70
NeoGuRi95 <jea5158@gmail.com>
2022-08-06 17:38:13 +0900

    운영용 DB 접속정보 추가, 톰캣7 메이븐 플로그인 배포 설정

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,10 +1,28 @@
 package com.ll.exam;
 
 public class App {
-    public static final String BASE_PACKAGE_PATH = "com.ll.exam";
-    public static final String DB_HOST = "localhost";
-    public static final int DB_PORT = 3306;
-    public static final String DB_ID = "root";
-    public static final String DB_PASSWORD = "0000";
-    public static final String DB_NAME = "blog";
+    // 테스트 설정
+    public static String BASE_PACKAGE_PATH = "com.ll.exam";
+    public static String DB_HOST = "localhost";
+    public static int DB_PORT = 3306;
+    public static String DB_ID = "root";
+    public static String DB_PASSWORD = "0000";
+    public static String DB_NAME = "blog";
+    public static boolean isProd = false;
+
+    static {
+        String profilesActive = System.getenv("SPRING_PROFILES_ACTIVE");
+
+        if (profilesActive != null && profilesActive.equals("production")) {
+            isProd = true;
+        }
+        // 운영 설정
+        if (isProd) {
+            DB_HOST = "172.17.0.1";
+            DB_PORT = 3306;
+            DB_ID = "jea";
+            DB_PASSWORD = "0000";
+            DB_NAME = "site1";
+        }
+    }
 }
